9c05c8ac7ff8b1f6f77226eb86d335bbf5ef776e
Mitchell Luhm <luhmx009@umn.edu>
2018-08-02 15:19:11 -0500

    initial commit

---
+++ TimeMoney/TimeMoneyApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TimemoneyappConfig(AppConfig):
+    name = 'TimeMoneyApp'
---
+++ TimeMoney/TimeMoneyApp/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from TimeMoneyApp import views
+
+urlpatterns = [
+    url(r'^$', views.index, name='index')
+]
